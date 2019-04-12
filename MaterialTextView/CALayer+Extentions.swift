eee28565f81fa7a64ee59577ec0e123c28ca4e7a
Motyzhenkov Mikhail <m.motyzhenkov@qiwi.ru>
2019-04-12 16:15:27 +0300

    fix animations

--- MaterialTextView/CALayer+Extentions.swift
+++ MaterialTextView/CALayer+Extentions.swift
@@ -57,18 +57,17 @@ internal extension CATextLayer {
 		CATransaction.begin()
 		CATransaction.setAnimationDuration(animationDuration)
 		CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear))
+		
+		if let newColor = newColor {
+			self.foregroundColor = newColor
+		}
 
 		switch animationType {
 		case .skip:
 			break
 		case .identity:
 			if CATransform3DEqualToTransform(self.transform, CATransform3DIdentity) { break }
-			let transform = CATransform3DIdentity
-			let transformAnimation = CABasicAnimation(keyPath: #keyPath(CATextLayer.transform))
-			transformAnimation.fromValue = self.transform
-			transformAnimation.toValue = transform
-			self.transform = transform
-			self.add(transformAnimation, forKey: #keyPath(CATextLayer.transform))
+			self.transform = CATransform3DIdentity
 		case .scaleAndTranslate(let scale):
 			if scale == 1 {
 				break
@@ -76,24 +75,10 @@ internal extension CATextLayer {
 			let startFrame = self.frame
 			let dx: CGFloat = -startFrame.width * (1-scale)/2 - (startFrame.origin.x - newFrame.origin.x)
 			let dy = -startFrame.height * (1-scale)/2 - (startFrame.origin.y - newFrame.origin.y)
-			let transform = CATransform3D(m11: scale, m12: 0, m13: 0, m14: 0,
-										  m21: 0, m22: scale, m23: 0, m24: 0,
-										  m31: 0, m32: 0, m33: 1, m34: 0,
-										  m41: dx, m42: dy, m43: 0, m44: 1)
-
-			let transformAnimation = CABasicAnimation(keyPath: #keyPath(CATextLayer.transform))
-			transformAnimation.fromValue = self.transform
-			transformAnimation.toValue = transform
-			self.transform = transform
-			self.add(transformAnimation, forKey: #keyPath(CATextLayer.transform))
-		}
-
-		if let newColor = newColor {
-			let colorAnimation = CABasicAnimation(keyPath: #keyPath(CATextLayer.foregroundColor))
-			colorAnimation.fromValue = self.foregroundColor
-			colorAnimation.toValue = newColor
-			self.foregroundColor = newColor
-			self.add(colorAnimation, forKey: #keyPath(CATextLayer.foregroundColor))
+			self.transform = CATransform3D(m11: scale, m12: 0, m13: 0, m14: 0,
+										   m21: 0, m22: scale, m23: 0, m24: 0,
+										   m31: 0, m32: 0, m33: 1, m34: 0,
+										   m41: dx, m42: dy, m43: 0, m44: 1)
 		}
 
 		CATransaction.commit()
