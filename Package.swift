4eda85aa8250d06e37db5f9875256ebe1ac00a53
Alexander Desyatov <a.desyatov@qiwi.com>
2020-12-16 10:14:29 +0300

    Update package

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "MaterialTextView",
     platforms: [
-            .iOS(.v9),
-        ],
+        .iOS(.v9),
+    ],
     products: [
         // The external product of our package is an importable
         // library that has the same name as the package itself:
@@ -23,5 +23,9 @@ let package = Package(
         // Our package contains two targets, one for our library
         // code, and one for our tests:
         .target(name: "MaterialTextView", dependencies: ["FormattableTextView"], path: "MaterialTextView", exclude: ["Info.plist"])
+    ],
+    
+    swiftLanguageVersions: [
+        .v5
     ]
 )
