2c50456f2892a47e8145767104f8a6936e2e4870
Motyzhenkov Mikhail <m.motyzhenkov@qiwi.com>
2021-09-20 11:39:01 +0300

    add viewModel

---
+++ MaterialTextView/MaterialTextViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MaterialTextViewModel.swift
+//  MaterialTextView
+//
+//  Created by Mikhail Motyzhenkov on 03.09.2021.
+//  Copyright © 2021 QIWI. All rights reserved.
+//
+
+import Foundation
+
+public final class MaterialTextViewModel {
+	
+}
