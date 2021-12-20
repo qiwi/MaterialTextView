a0eb8fdcc666248957b7dae22fdf83c5c02cfeab
Gazovskiy Pavel <p.gazovskiy@qiwi.com>
2021-12-20 15:55:58 +0300

    fixes

--- Package.swift
+++ Package.swift
@@ -1,34 +1,35 @@
-	// swift-tools-version:5.3
+// swift-tools-version:5.3
 
-	import PackageDescription
+import PackageDescription
 
-	let package = Package(
-		name: "MaterialTextView",
-		platforms: [
-			.iOS(.v9),
-		],
-		products: [
-			// The external product of our package is an importable
-			// library that has the same name as the package itself:
-			.library(
-				name: "MaterialTextView",
-				type: .static,
-				targets: ["MaterialTextView"]
-			)
-		],
-		
-		dependencies: [
-			.package(url: "https://github.com/qiwi/FormattableTextView.git",
-						.upToNextMajor(from: "2.7.0")
-			)
-		],
-		targets: [
-			// Our package contains two targets, one for our library
-			// code, and one for our tests:
-			.target(name: "MaterialTextView", dependencies: ["FormattableTextView"], path: "MaterialTextView", exclude: ["Info.plist"])
-		],
-		
-		swiftLanguageVersions: [
-			.v5
-		]
-	)
+let package = Package(
+	name: "MaterialTextView",
+	platforms: [
+		.iOS(.v9),
+	],
+	products: [
+		// The external product of our package is an importable
+		// library that has the same name as the package itself:
+		.library(
+			name: "MaterialTextView",
+			type: .static,
+			targets: ["MaterialTextView"]
+		)
+	],
+
+	dependencies: [
+		.package(
+			url: "https://github.com/qiwi/FormattableTextView.git",
+			.upToNextMajor(from: "2.7.0")
+		)
+	],
+	targets: [
+		// Our package contains two targets, one for our library
+		// code, and one for our tests:
+		.target(name: "MaterialTextView", dependencies: ["FormattableTextView"], path: "MaterialTextView", exclude: ["Info.plist"])
+	],
+
+	swiftLanguageVersions: [
+		.v5
+	]
+)
